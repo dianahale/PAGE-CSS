cd60445633383d2b797b5177c152d2fdc60b43eb
Diana <dianalaura.alejo@outlook.com>
2024-02-06 02:49:00 -0600

    Add Register Form

---
+++ js/account.js
@@ -0,0 +1,36 @@
+document.getElementById('registroForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevenir el envío del formulario por defecto
+
+    // Obtener datos del formulario
+    const formData = new FormData(this);
+
+    // Convertir datos a formato JSON
+    const jsonData = {};
+    formData.forEach((value, key) => {
+      jsonData[key] = value;
+    });
+
+    // Realizar solicitud POST al microservicio
+    fetch('https://localhost:44306/api/Account', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(jsonData)
+    })
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Error en la solicitud');
+        }
+        return response.json();
+    })
+    .then(data => {
+        alert('Registro exitoso');
+        // Limpiar el formulario
+        this.reset();
+    })
+    .catch(error => {
+        alert('Hubo un error en el registro');
+    });
+  });
+
