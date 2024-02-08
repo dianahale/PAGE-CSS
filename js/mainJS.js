a13dd7c942a95901367b858e3c6ec3f3ce4507bb
Diana <dianalaura.alejo@outlook.com>
2024-02-08 03:40:53 -0600

    Responsive

--- js/mainJS.js
+++ js/mainJS.js
@@ -15,11 +15,29 @@
             if(answer.clientHeight === 0){
                 height = answer.scrollHeight;  //obtener el alto minimo para que se muestre
             }
-
             answer.style.height = `${height}px`; //asignar la altura minima para poder mostrarse
-
-
             
         });
     });
 })();
+
+(function(){
+    const openButton = document.querySelector('.nav__menu');
+    const menu = document.querySelector('.nav__link');
+    const closeMenu = document.querySelector('.nav__close');
+
+    openButton.addEventListener('click', ()=>{
+        menu.classList.add('nav__link--show');
+    });
+
+    closeMenu.addEventListener('click', ()=>{
+        menu.classList.remove('nav__link--show');
+    });
+})();
+
+
+
+
+
+
+
